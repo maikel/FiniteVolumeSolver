f96a99bf8ad9156f98ec3b856614dee805745b19
Maikel Nadolski <maikel.nadolski@gmail.com>
2019-04-18 13:47:37 +0200

    Add Dockerfiles. Add Documentation.

--- include/fub/CartesianCoordinates.hpp
+++ include/fub/CartesianCoordinates.hpp
@@ -26,24 +26,60 @@
 
 namespace fub {
 
+/// This class handles uniform cartesian cell coordinates.
+///
+/// It stores lower and upper node coordinate bounds, cell widths and cell
+/// extents. You can also compute cell and nodes coordinates with this class.
 class CartesianCoordinates {
 public:
+  /// This constructs a coordinate object given domain bounds, cell width and
+  /// cell extents.
+  ///
+  /// \param[in] lower Coordinates of lowest node.
+  /// \param[in] upper Coordinates of the highest node.
+  /// \param[in] dx Widths of cells
+  /// \param[in] extents Number of cells for all coordinate direction.
   CartesianCoordinates(const Eigen::Vector3d& lower,
                        const Eigen::Vector3d& upper, const Eigen::Vector3d& dx,
                        dynamic_extents<3> extents) noexcept;
 
+  /// Returns coordinates of the mid point of a cell in given cell indices.
+  ///
+  /// \param[in] i The cell index in X direction
+  /// \param[in] j The cell index in Y direction
+  /// \param[in] k The cell index in Z direction
+  ///
+  /// \see Same as CartesianCoordinates::CellCoordinates(i, j, k)
   Eigen::Vector3d operator()(std::ptrdiff_t i, std::ptrdiff_t j = 0,
                              std::ptrdiff_t k = 0) const;
 
+  /// Returns coordinates of the mid point of a cell in given cell indices.
+  ///
+  /// \param[in] i The cell index in X direction
+  /// \param[in] j The cell index in Y direction
+  /// \param[in] k The cell index in Z direction
+  ///
+  /// \return A three-dimensional Eigen::Vector3d.
+  ///
+  /// \see Same as CartesianCoordinates::operator()(i, j, k)
   Eigen::Vector3d CellCoordinates(std::ptrdiff_t i, std::ptrdiff_t j,
                                   std::ptrdiff_t k) const;
 
+  /// Returns coordinates of a nodes in given nodes indices.
+  ///
+  /// \param[in] i The cell index in X direction
+  /// \param[in] j The cell index in Y direction
+  /// \param[in] k The cell index in Z direction
   Eigen::Vector3d NodeCoordinates(std::ptrdiff_t i, std::ptrdiff_t j,
                                   std::ptrdiff_t k) const;
 
+  /// Returns the cell width size.
   const Eigen::Vector3d& dx() const noexcept { return dx_; }
 
+  /// Returns the most upper node coordinates (inclusive).
   const Eigen::Vector3d& GetUpper() const noexcept { return upper_; }
+
+  /// Returns the lowest node coordinates (inclusive).
   const Eigen::Vector3d& GetLower() const noexcept { return lower_; }
 
 private:
