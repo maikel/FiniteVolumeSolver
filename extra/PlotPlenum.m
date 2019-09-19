35f6f78860eec5ba8efa1216f889c02d075613a0
Maikel Nadolski <maikel.nadolski@fu-berlin.de>
2019-09-19 14:55:23 +0200

    eckhaus specific settings

--- extra/PlotPlenum.m
+++ extra/PlotPlenum.m
@@ -1,19 +1,19 @@
-pathToPlenum = '/Volumes/Maikel_Intenso/FiniteVolumeSolver_Build_3d/MultiTube/Matlab/Plenum_y0';
-videoPath = '/Volumes/Maikel_Intenso/Plenum_y0.avi';
+pathToPlenum = '/group/ag_klima/SFB1029_C01/HLRN/MultiTube/Plenum_x5';
+videoPath = '/srv/public/Plenum_x5.avi';
 
 [X, Y, Z, time, data] = ReadPlenumData(pathToPlenum);
 
 %%
 
-f = figure(1);
-MakeVideo(videoPath, X, Z, time, data);
+f = figure('visible', 'off');
+MakeVideo(videoPath, Y, Z, time, data);
 
 function MakeVideo(out, X, Y, time, data)
     v = VideoWriter(out);
     open(v);
     nx = length(X);
     ny = length(Y);
-    for k = 340:length(time)
+    for k = 1:length(time)
 %        subplot(2, 1, 1);       
 %        imagesc(X, Y, reshape(data.rho(:, 1, :, k), nx, ny)');
 %        axis equal;
@@ -21,18 +21,18 @@ function MakeVideo(out, X, Y, time, data)
 %        colorbar;
 %        caxis([1.0 1.7])
 %        ylabel('Density [kg / m^3]');
-       
+       fprintf('Write Frame %d.\n', k);
        subplot(2, 1, 1);
-       imagesc(X, Y, reshape(data.p(:, 1, :, k), nx, ny)');
+       imagesc(X, Y, reshape(data.p(1, :, :, k), nx, ny)');
        axis equal;
        colorbar;
-       caxis([8e4 2e5])
+       caxis([8e4 1.3e5])
        set(gca, 'Ydir', 'normal');
        ylabel('Pressure [Pa]');
        title(sprintf('time = %fs', time(k)));
        
        subplot(2, 1, 2);
-       imagesc(X, Y, reshape(data.rhou(:, 1, :, k, 1) ./ data.rho(:, 1, :, k), nx, ny)');
+       imagesc(X, Y, reshape(data.rhou(1, :, :, k, 1) ./ data.rho(1, :, :, k), nx, ny)');
        axis equal;
        colorbar;
        caxis([0 80])
