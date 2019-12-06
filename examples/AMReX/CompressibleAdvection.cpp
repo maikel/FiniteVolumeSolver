c876ba4de67700b30e4603ed8243e5d8c9cdefbe
Maikel Nadolski <maikel.nadolski@fu-berlin.de>
2019-12-06 07:46:55 +0100

    Complete the Driver for CompressibleAdvection

--- examples/AMReX/CompressibleAdvection.cpp
+++ examples/AMReX/CompressibleAdvection.cpp
@@ -32,13 +32,25 @@ struct InitialData {
       const amrex::Box& box = mfi.tilebox();
       fub::View<Complete> states = fub::amrex::MakeView<Complete>(fab, equation, box);
       fub::ForEachIndex(fub::Box<0>(states), [&](int i, int j) {
-        states.density(i, j) = 1.0;
-        states.momentum(i, j, 0) = 0.0;
-        states.momentum(i, j, 1) = 0.0;
-        states.PTdensity(i, j) = 1.0;
-        states.velocity(i, j, 0) = 0.0;
-        states.velocity(i, j, 1) = 0.0;
-        states.PTinverse(i, j) = 1.0;
+        const double x = geom.CellCenter(i, 0);
+        const double y = geom.CellCenter(j, 0);
+        if (x*x + y*y < 0.25 * 0.25) {
+          states.density(i, j) = 42.0;
+          states.momentum(i, j, 0) = 0.0;
+          states.momentum(i, j, 1) = 0.0;
+          states.PTdensity(i, j) = 1.0;
+          states.velocity(i, j, 0) = 0.0;
+          states.velocity(i, j, 1) = 0.0;
+          states.PTinverse(i, j) = 42.0;
+        } else {
+          states.density(i, j) = 24.0;
+          states.momentum(i, j, 0) = 0.0;
+          states.momentum(i, j, 1) = 0.0;
+          states.PTdensity(i, j) = 1.0;
+          states.velocity(i, j, 0) = 0.0;
+          states.velocity(i, j, 1) = 0.0;
+          states.PTinverse(i, j) = 24.0;
+        }
       });
     });
   }
@@ -46,16 +58,17 @@ struct InitialData {
 
 int main()
 {
+  std::chrono::steady_clock::time_point wall_time_reference =
+      std::chrono::steady_clock::now();
+
   fub::amrex::ScopeGuard guard{};
   fub::amrex::DataDescription desc{};
   desc.n_state_components = 7;
-  desc.n_node_components = 0;
-  desc.n_face_components = 1;
+  desc.n_cons_components = 4;
 
   fub::amrex::CartesianGridGeometry geometry{};
   geometry.cell_dimensions = std::array<int, 2>{128, 128};
-  geometry.coordinates = amrex::RealBox({-1.0, -1.0},
-                                        {+1.0, +1.0});
+  geometry.coordinates = amrex::RealBox({-1.0, -1.0}, {+1.0, +1.0});
   geometry.periodicity = std::array<int, 2>{1, 1};
 
   fub::amrex::PatchHierarchyOptions hier_opts;
@@ -64,8 +77,44 @@ int main()
 
   fub::amrex::PatchHierarchy hierarchy(desc, geometry, hier_opts);
 
-  fub::amrex::GriddingAlgorithm grid(std::move(hierarchy), InitialData{}, fub::amrex::TagBuffer{0});
-  grid.InitializeHierarchy(0.0);
+  std::shared_ptr grid = std::make_shared<fub::amrex::GriddingAlgorithm>(std::move(hierarchy), InitialData{}, fub::amrex::TagBuffer{0});
+  grid->InitializeHierarchy(0.0);
 
-  fub::amrex::FluxMethod fm(fub::execution::simd, fub::CompressibleAdvectionFluxMethod<2>());
+  fub::CompressibleAdvection<2> equation{};
+
+  using namespace fub;
+  CompressibleAdvectionFluxMethod<2> flux_method;
+  flux_method.Pv_function_ = [](std::array<double, 2>, Duration, Direction dir) -> double {
+    switch (dir) {
+      case Direction::X:
+        return 1.0;
+      default:
+        return 0.0;
+    }
+  };
+
+  auto tag = fub::execution::seq;
+  fub::amrex::HyperbolicMethod method{
+      fub::amrex::FluxMethod(tag, flux_method),
+      fub::amrex::ForwardIntegrator(tag),
+      fub::amrex::Reconstruction(tag, equation)};
+
+  fub::DimensionalSplitLevelIntegrator level_integrator(
+      fub::int_c<2>, fub::amrex::IntegratorContext(grid, method, 3, 2),
+      fub::StrangSplitting());
+
+  fub::SubcycleFineFirstSolver solver(level_integrator);
+
+  using namespace std::literals::chrono_literals;
+  std::string base_name = "CompressibleAdvection";
+
+  fub::MultipleOutputs<fub::amrex::GriddingAlgorithm> output{};
+  output.AddOutput(fub::MakeOutput<fub::amrex::GriddingAlgorithm>(
+      {}, {0.1s}, fub::amrex::PlotfileOutput(equation, base_name)));
+
+  output(*solver.GetGriddingAlgorithm());
+  fub::RunOptions run_options{};
+  run_options.final_time = 3.0s;
+  run_options.cfl = 0.8;
+  fub::RunSimulation(solver, run_options, wall_time_reference, output);
 }
