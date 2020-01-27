8a037fb58b98ad546da3b2961368bc0ecf5888d1
Stefan Vater <svater@users.noreply.github.com>
2020-01-27 15:08:08 +0100

    adjusted python script for analysis

--- Plot_BK19.py
+++ Plot_BK19.py
@@ -12,26 +12,35 @@ from mpl_toolkits.axes_grid1 import AxesGrid
 if (not funcs.get_interactivity()):
   yt.toggle_interactivity()
 
-def Load(path):
-  #p = '{}/'.format(path)
-  p = path
-  shutil.copy2('extra/yt/WarpXHeader', p)
-  shutil.copy2('extra/yt/warpx_job_info', p)
-  return yt.load(p)
-
-def PlotVariables(base_dir, step_dir, timestep):
-
+def Load(base_dir, step_dir, timestep):
   path = os.path.join(base_dir, step_dir, 'plt{:09d}'.format(timestep))
-  ds   = Load(path)
+  shutil.copy2('extra/yt/WarpXHeader', path)
+  shutil.copy2('extra/yt/warpx_job_info', path)
+  return yt.load(path)
+
+def LoadPv(base_dir, step_dir, timestep):
+  path = os.path.join(base_dir, step_dir, 'Pv_plt{:09d}'.format(timestep))
+  shutil.copy2('extra/yt/WarpXHeader', path)
+  shutil.copy2('extra/yt/warpx_job_info', path)
+  return yt.load(path)
+
+def PlotVariables(base_dir, step_dir, timestep, fignum = -1):
+
+  ds = Load(base_dir, step_dir, timestep)
 
   variables = ['Density', 'PTdensity', 'Velocity_0', 'Velocity_1']
   p = yt.plot_2d(ds, variables, origin='native')
   p.set_buff_size(256) # to remove some interpolation errors
 
-  fig = plt.figure()
+  if (fignum == -1):
+    fig = plt.figure()
+  else:
+    fig = plt.figure(fignum)
+    fig.clf()
+
   for i, var in enumerate(variables):
-    ax = plt.subplot(2,3,i+1)
-    im = ax.imshow(np.array(p.frb[var]), origin='lower',extent=np.array(p.bounds))
+    ax = plt.subplot(2,4,i+1)
+    im = ax.imshow(np.array(p.frb[var]), origin='lower', extent=np.array(p.bounds))
     im.set_cmap('Blue-Red')
     plt.colorbar(im, ax=ax, shrink=.75)
     ax.set_title(var)
@@ -50,13 +59,72 @@ def PlotVariables(base_dir, step_dir, timestep):
   data_nd[ -1,:-1] = data[-1,:,2]
   data_nd[ -1, -1] = data[-1,-1,3]
 
-  ax = plt.subplot(2,3,5)
-  im = ax.imshow(data_nd, origin='lower', extent=extent)
+  ax = plt.subplot(2,4,5)
+  im = ax.imshow(data_nd.T, origin='lower', extent=extent)
   im.set_cmap('Blue-Red')
   plt.colorbar(im, ax=ax, shrink=.75)
   ax.set_title('pi')
 
-  fig.suptitle("timestep=%d, step=%s" %(timestep, step_dir))
+  #ds = LoadPv(base_dir, step_dir, timestep)
+
+  ##variables = ['Pv_0', 'Pv_1']
+  #variables = ['Pv_0']
+  #p = yt.plot_2d(ds, variables, origin='native')
+  #p.set_buff_size(256) # to remove some interpolation errors
+
+  #for i, var in enumerate(variables):
+    #ax = plt.subplot(2,4,i+6)
+    #im = ax.imshow(np.array(p.frb[var]), origin='lower', extent=np.array(p.bounds))
+    #im.set_cmap('Blue-Red')
+    #plt.colorbar(im, ax=ax, shrink=.75)
+    #ax.set_title(var)
+
+  ds = Load(base_dir, step_dir, timestep)
+  g = ds.index.grids[0]
+
+  data = np.squeeze(np.array(g['Pv_x']))
+
+  extent = [g.LeftEdge[0]  - g.dds[0]/2,
+            g.RightEdge[0] + g.dds[0]/2,
+            g.LeftEdge[1],
+            g.RightEdge[1]]
+
+  data_fc = np.zeros((g.shape[0]+1, g.shape[1]))
+  data_fc[:-1,:] = data[:,:,0]
+  data_fc[-1,:] = data[-1,:,1]
+  div = data[:,:,1] - data[:,:,0]
+
+  ax = plt.subplot(2,4,6)
+  im = ax.imshow(data_fc.T, origin='lower', extent=extent)
+  im.set_cmap('Blue-Red')
+  plt.colorbar(im, ax=ax, shrink=.75)
+  ax.set_title('Pv_x')
+
+  data = np.squeeze(np.array(g['Pv_y']))
+
+  extent = [g.LeftEdge[0],
+            g.RightEdge[0],
+            g.LeftEdge[1]  - g.dds[1]/2,
+            g.RightEdge[1] + g.dds[1]/2]
+
+  data_fc = np.zeros((g.shape[0], g.shape[1]+1))
+  data_fc[:,:-1] = data[:,:,0]
+  data_fc[:,-1] = data[:,-1,1]
+  div = div + data[:,:,1] - data[:,:,0]
+
+  ax = plt.subplot(2,4,7)
+  im = ax.imshow(data_fc.T, origin='lower', extent=extent)
+  im.set_cmap('Blue-Red')
+  plt.colorbar(im, ax=ax, shrink=.75)
+  ax.set_title('Pv_y')
+
+  ax = plt.subplot(2,4,8)
+  im = ax.imshow(div.T, origin='lower', extent=extent)
+  im.set_cmap('Blue-Red')
+  plt.colorbar(im, ax=ax, shrink=.75)
+  ax.set_title('div(Pv)')
+
+  fig.suptitle("timestep=%d, t=%.4f, stage=%s" %(timestep, ds.current_time.value, step_dir[27:]))
   plt.show()
 
 timestep = 1
@@ -70,12 +138,12 @@ step_dirs = ['BK19_Pseudo_Incompressible-pre-step',
              'BK19_Pseudo_Incompressible-advect-backward-forward-advect-backward']
 
 
-PlotVariables(base_dir, step_dirs[0], timestep)
-PlotVariables(base_dir, step_dirs[1], timestep)
-PlotVariables(base_dir, step_dirs[2], timestep)
-PlotVariables(base_dir, step_dirs[3], timestep)
-PlotVariables(base_dir, step_dirs[4], timestep)
-PlotVariables(base_dir, step_dirs[5], timestep)
+PlotVariables(base_dir, step_dirs[0], timestep, 1)
+PlotVariables(base_dir, step_dirs[1], timestep, 2)
+PlotVariables(base_dir, step_dirs[2], timestep, 3)
+PlotVariables(base_dir, step_dirs[3], timestep, 4)
+PlotVariables(base_dir, step_dirs[4], timestep, 5)
+PlotVariables(base_dir, step_dirs[5], timestep, 6)
 
 #ds0 = yt.load(paths[0])
 #ds1 = yt.load(paths[1])
