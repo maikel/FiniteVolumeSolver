cc08b0b6f09b5ed7e884dafed145296ac62aae02
Maikel Nadolski <maikel.nadolski@gmail.com>
2020-01-30 01:41:49 +0100

    Update LinearShock3d for ProgramOptions

--- examples/AMReX/EB/LinearShock3d.py
+++ examples/AMReX/EB/LinearShock3d.py
@@ -1,29 +1,37 @@
 RunOptions = {
   'cfl': 0.5,
-  'final_time': 0.02,
-  'max_cycles': 10
+  'final_time': 0.001,
+  'max_cycles': 0
 }
 
 GridGeometry = {
-  'cell_dimensions': [32, 32, 32],
+  'cell_dimensions': [64, 64, 64],
   'coordinates': {
-    'lower': [-0.03, -0.15, -0.15],
-    'upper': [0.27, 0.15, 0.15],
+    'lower': [-0.06, -0.15, -0.15],
+    'upper': [0.24, 0.15, 0.15],
   },
   'periodicity': [0, 0, 0]
 }
 
 PatchHierarchy = {
-  'max_number_of_levels': 1, 
-  'blocking_factor': [16, 16, 16],
+  'max_number_of_levels': 2, 
+  'blocking_factor': [8, 8, 8],
   'max_grid_size': [32, 32, 32],
-  'ngrow_eb_level_set': 5
+  'ngrow_eb_level_set': 3
+}
+
+InitialCondition = {
+  'left': {
+    'pressure': 3.0 * 101325.0
+  }, 'right': {
+    'pressure': 1.0 * 101325.0
+  }
 }
 
 Output = { 
   'outputs': [{
     'type': 'Plotfile',
-    'directory': 'MultiTube/',
+    'directory': 'LinearShock3d/',
     # 'intervals': [1e-4],
     'frequencies': [1],
   }, {
