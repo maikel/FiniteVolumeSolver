275726483a160e8f480fac9c33006056c2c00219
Maikel Nadolski <maikel.nadolski@gmail.com>
2020-01-31 13:48:07 +0100

    Update configuration with new options

--- extra/BK19_Input.py
+++ extra/BK19_Input.py
@@ -1,26 +1,39 @@
 BK19LevelIntegrator = {
-    'mlmg_tolerance_rel': 1e-5,
-    'mlmg_tolerance_abs': 1e-5,
-    'mlmg_verbose': 1,
-    'mlmg_max_iter': 1,
-    'bottom_tolerance_rel': 1e-7,
+    'mlmg_tolerance_rel': -1,
+    'mlmg_tolerance_abs': 1e-12,
+    'mlmg_verbose': 0,
+    'mlmg_max_iter': 100,
+    'bottom_tolerance_rel': 1e-6,
     'bottom_tolerance_abs': -1.0,
-    'bottom_verbose': 1,
-    'bottom_max_iter': 300
+    'bottom_verbose': 0,
+    'bottom_max_iter': 50
 }
 
 GridGeometry = {
+    # 'cell_dimensions': [32, 32],
     'cell_dimensions': [32, 32],
-#    'cell_dimensions': [64, 64],
     'x_lower': [0.0, 0.0],
     'x_upper': [1.0, 1.0],
     'periodicity': [1, 1]
 }
 
-PatchHierarchyOptions = {
+PatchHierarchy = {
     'max_number_of_levels': 1,
     'refine_ratio': [2, 2],
     'blocking_factor': [32, 32],
-#    'max_grid_size': [64, 64]
-    'max_grid_size': [32, 32]
+    'max_grid_size': [256, 256]
+}
+
+RunOptions = {
+    'cfl': 0.45,
+    'final_time': 1.0,
+}
+
+Output = {
+    'outputs': [
+        {
+            'type': 'Plotfile',
+            'intervals': [5e-2]
+        },
+    ]
 }
