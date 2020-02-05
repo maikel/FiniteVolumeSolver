9f5b11c69618d242cfd19b442899be4c5704e2e5
Maikel Nadolski <maikel.nadolski@gmail.com>
2020-02-05 14:01:48 +0100

    Super safe cutcell stabilisation

--- src/cutcell_method/KbnStabilisation.cpp
+++ src/cutcell_method/KbnStabilisation.cpp
@@ -46,6 +46,12 @@ template <typename T> struct CutCellGeometry {
   T centerR;
 };
 
+Vc::double_v clamp(Vc::double_v x, Vc::double_v lo, Vc::double_v hi) noexcept {
+  where(x < lo, x) = lo;
+  where(hi < x, x) = hi;
+  return x;
+}
+
 void ComputeStableFluxes_Row(const Fluxes<double*, const double*>& fluxes,
                              const CutCellGeometry<const double*>& geom, int n,
                              Duration /* dt */, double /* dx */) {
@@ -54,15 +60,17 @@ void ComputeStableFluxes_Row(const Fluxes<double*, const double*>& fluxes,
   for (face = 0; face + simd_width <= n; face += simd_width) {
     const Vc::double_v centerL(geom.centerL + face, Vc::Unaligned);
     const Vc::double_v centerR(geom.centerR + face, Vc::Unaligned);
-    const Vc::double_v dL = Vc::double_v(0.5) - centerL;
-    const Vc::double_v dR = Vc::double_v(0.5) + centerR;
+    const Vc::double_v dL = clamp(Vc::double_v(0.5) - centerL, Vc::double_v(0.0), Vc::double_v(1.0));
+    const Vc::double_v dR = clamp(Vc::double_v(0.5) + centerR, Vc::double_v(0.0), Vc::double_v(1.0));
+    const Vc::double_v dLm1 = clamp(Vc::double_v(1.0) - dL, Vc::double_v(0.0), Vc::double_v(1.0));
+    const Vc::double_v dRm1 = clamp(Vc::double_v(1.0) - dR, Vc::double_v(0.0), Vc::double_v(1.0));
 
     const Vc::double_v f(fluxes.regular + face, Vc::Unaligned);
     const Vc::double_v fbL(fluxes.boundaryL + face, Vc::Unaligned);
     const Vc::double_v fbR(fluxes.boundaryR + face, Vc::Unaligned);
-    const Vc::double_v fsL = dL * f + (Vc::double_v(1.0) - dL) * fbL;
-    const Vc::double_v fsR = dR * f + (Vc::double_v(1.0) - dR) * fbR;
-    
+    const Vc::double_v fsL = dL * f + dLm1 * fbL;
+    const Vc::double_v fsR = dR * f + dRm1 * fbR;
+
     const Vc::double_v betaL(geom.betaL + face, Vc::Unaligned);
     const Vc::double_v betaR(geom.betaR + face, Vc::Unaligned);
     const Vc::double_v betaUS(geom.betaUS + face, Vc::Unaligned);
