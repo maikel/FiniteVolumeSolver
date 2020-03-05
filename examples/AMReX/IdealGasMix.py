01d1fd127024db3bca4fa62d9e624a45b14e67d5
Maikel Nadolski <maikel.nadolski@gmail.com>
2020-03-05 09:58:31 +0100

    Fix IdealGasMix configuration

--- examples/AMReX/IdealGasMix.py
+++ examples/AMReX/IdealGasMix.py
@@ -5,37 +5,36 @@ RunOptions = {
   'final_time': 0.04,
 }
 
-#CartesianGridGeometry = {
-#  'cell_dimensions': [800, 1],
-#  'coordinates': {
-#    'lower': [-1.50, -0.015],
-#    'upper': [-0.00, +0.015],
-#  },
-#  'periodicity': [0, 0]
-#}
-
 CartesianGridGeometry = {
-  'cell_dimensions': [800, 1, 1],
-  'coordinates': {
-    'lower': [-1.50, -0.015, -0.015],
-    'upper': [-0.00, +0.015, +0.015],
-  },
-  'periodicity': [0, 0, 0]
+ 'cell_dimensions': [800, 1],
+ 'coordinates': {
+   'lower': [-1.50, -0.015],
+   'upper': [-0.00, +0.015],
+ },
+ 'periodicity': [0, 0]
 }
 
-#PatchHierarchy = {
-#  'max_number_of_levels': 1, 
-#  'refine_ratio': [2, 1],
-#  'blocking_factor': [8, 1],
-#  'max_grid_size': [120, 1],
-#}
+# CartesianGridGeometry = {
+#   'cell_dimensions': [800, 1, 1],
+#   'coordinates': {
+#     'lower': [-1.50, -0.015, -0.015],
+#     'upper': [-0.00, +0.015, +0.015],
+#   }
+# }
 
 PatchHierarchy = {
  'max_number_of_levels': 1, 
- 'refine_ratio': [2, 1, 1],
- 'blocking_factor': [8, 1, 1],
- 'max_grid_size': [120, 1, 1],
+ 'refine_ratio': [2, 1],
+ 'blocking_factor': [8, 1],
+ 'max_grid_size': [120, 1],
 }
+
+# PatchHierarchy = {
+#  'max_number_of_levels': 1, 
+#  'refine_ratio': [2, 1, 1],
+#  'blocking_factor': [8, 1, 1],
+#  'max_grid_size': [120, 1, 1],
+# }
 
 PressureValveBoundary = {
   'efficiency': 1.0,
@@ -50,13 +49,13 @@ PressureValveBoundary = {
   'equivalence_ratio': 1.0,
   'massflow_boundary': {
     'coarse_inner_box': { 
-      'lower': [0, 0, 0], 
-      'upper': [1, 0, 0] 
+      'lower': [0, 0], 
+      'upper': [1, 0] 
     },
     'side': 0,
     'direction': 0,
     'required_massflow': 120.0 / 3600.0,
-    'surface_area': 2.0 * math.pi * 0.015 * 0.015
+    'surface_area': math.pi * 0.015 * 0.015
   }
 }
 
@@ -76,7 +75,7 @@ Output = {
   'outputs': [{
     'type': 'HDF5',
     'path': './IdealGasMix.h5',
-    'intervals': [1e-5],
+    'intervals': [5e-5],
   }, {
     'type': 'CounterOutput',
     'frequencies': [100]
