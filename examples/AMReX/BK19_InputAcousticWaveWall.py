acb0c098c3b6148d2a26e949d64a5c41914ac3d6
Stefan Vater <svater@mi.fu-berlin.de>
2020-05-25 09:28:41 +0200

    added parameter file for wall bc

---
+++ examples/AMReX/BK19_InputAcousticWaveWall.py
@@ -0,0 +1,48 @@
+BK19LevelIntegrator = {
+    'do_initial_projection': True,
+    'mlmg_tolerance_rel': 1e-10,
+    'mlmg_tolerance_abs': 1e-10,
+    'mlmg_verbose': 2,
+    'mlmg_max_iter': 100,
+    'bottom_tolerance_rel': 1e-4,
+    'bottom_tolerance_abs': -1.0,
+    'bottom_verbose': 2,
+    'bottom_max_iter': 200
+}
+
+GridGeometry = {
+    'cell_dimensions': [64, 64, 64],
+    #'cell_dimensions': [32, 32, 32],
+    'coordinates': {
+      'lower': [0.0, 0.0, 0.0],
+      'upper': [1.0, 1.0, 1.0]
+    },
+    'periodicity': [1, 0, 1]
+}
+
+PatchHierarchy = {
+    'max_number_of_levels': 1,
+    'refine_ratio': [2, 2, 2],
+    #'blocking_factor': [64, 64, 64],
+    'blocking_factor': [32, 32, 32],
+    'max_grid_size': [256, 256, 256]
+}
+
+RunOptions = {
+    'cfl': 0.45,
+    'final_time': 0.002,
+}
+
+Output = {
+    'outputs': [
+        {
+            'type': 'DebugOutput',
+            'directory': './Debug/'
+        },
+        {
+            'type': 'Plotfile',
+            #'intervals': [5e-5]
+            'intervals': [2.5e-05]
+        },
+    ]
+}
