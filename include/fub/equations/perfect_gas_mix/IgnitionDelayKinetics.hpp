086481e74c122d1af7dcffd3ef6199e4b565462a
Christian Zenker <zenkechr@b-tu.de>
2021-01-13 19:14:51 +0100

    [WIP] more documention in IgnitionDelayKinetics

--- include/fub/equations/perfect_gas_mix/IgnitionDelayKinetics.hpp
+++ include/fub/equations/perfect_gas_mix/IgnitionDelayKinetics.hpp
@@ -32,19 +32,82 @@
 
 namespace fub::perfect_gas_mix {
 
-/// The options for the IgnitionDelay model.
+///@{
+/// \brief The options for the IgnitionDelay model.
+///
+/// Factors determining lambda for the radical reaction:
+/// - Yign
+/// - Yinit,
+/// - tau.
+///
+/// this shouldn't need any modifying ever, since the model is scaled to
+/// have ignition delay times of order unity.
+///
+/// Factors determining the temperature dependence in the radical buildup
+/// reaction:
+/// - R_B,
+/// - R_C,
+/// - R_E.
+///
+/// Shouldn't need modifying neither, but can be used to make the
+/// NTC region smaller (e.g. currently |1.0 - 1.4| -> 400 K)
+///
+/// Factors determining the secondary temperature dependence in the radical
+/// buildup reaction:
+/// - R2_A
+/// - R2_C,
+/// - R2_E.
+///
+/// This one is interesting. It controls the temperature dependence of the
+/// ignition delay time, as a perturbation around unity. Via R2_A = 0, this
+/// can be disabled for completely homogeneous reactions.
+/// Modifying R2_A has a visible impact on the pressure waves' slope in the
+/// heatmaps: Larger values make the lines more even.
+/// It's a good idea to choose A/C small and E large to retain the overall
+/// ignition delay time behaviour.
+///
+/// Factors determining the heat releasing reaction:
+/// - E,
+/// - C,
+/// -Tdiff.
+///
+/// This controls the exitation time. If E < .9, excitation time is
+/// sufficiently small that combustion is homogeneous. For E > .9,
+/// it is no more (at least not reliably).
+///@}
 struct IgnitionDelayKineticsOptions {
-  double Yign{0.2}; /// test
+  // Factors determining lambda for the radical reaction:
+  /// Y_fuel below which low temperature mech. kicks in
+  double Yign{0.2};
+  /// Initial Y_fuel
   double Yinit{1.0};
+  /// Ignition delay time
   double tau{1.0};
+
+  // Factors determining the temperature dependence in the radical buildup
+  // reaction:
   double R_B{0.};
   double R_C{70.};
   double R_E{1.2};
+
+  // Factors determining the secondary temperature dependence in the radical
+  // buildup reaction:
   double R2_A{0.};
   double R2_C{5.};
   double R2_E{1.5};
+
+  // Factors determining the heat releasing reaction:
+
+  /// T_ign for Arrhenius kinetics, makes excitation time of
+  /// order 10^-2, i.e. ~10µs, if set to .5 (or .9)
   double E{0.5};
+  /// Factor that controls how sharp the activation energy barrier
+  /// is. Many global reactions have A = 10^10, dedimensionalize
+  /// by multiplication with τ this gives C=16 We use 7 because
+  /// this models excitation times only, and excitation times do
+  /// not strongly depend on temperature.
   double C{7.};
+  /// Heat release. T0 ≈ 1000 K, so this leads to T_0 + 2500 K
   double Tdiff{2.7};
 };
 
