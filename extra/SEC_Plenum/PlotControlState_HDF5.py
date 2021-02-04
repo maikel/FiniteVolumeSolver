0c8b551d343b4b02213be064e2858a7068ef907b
Christian Zenker <Christian.Zenker@b-tu.de>
2021-02-04 16:15:23 +0100

    improve python script

--- extra/SEC_Plenum/PlotControlState_HDF5.py
+++ extra/SEC_Plenum/PlotControlState_HDF5.py
@@ -24,7 +24,7 @@ inputFilePath = FVS_path+"/examples/AMReX/EB/2D/"
 
 
 sys.path.append(inputFilePath)
-from SEC_Plenum_Arrhenius import t_ref, ControlOptions
+from SEC_Plenum_Arrhenius import t_ref, ControlOptions, T_ref
 
 controlState = "{}/ControlState.h5".format(dataPath)
 outPath = dataPath
@@ -41,20 +41,39 @@ def getSubKeyList(substring):
    return [ key for key in datas_dict.keys() if substring in key ]
    
 f, axs = plt.subplots(nrows=3, ncols=2, figsize=(17/2,20/2) )
-f.suptitle('control_state')
+f.suptitle('Control Station')
 
-plotKeyList = ['mass_flow', 'power', 'pressure', 'temperature', 'rpm', 'fuel']
+plotKeyList = ['mass_flow', 'power', 'pressure', 'temperature', 'rpm', 'fuel_consumption_rate']
 
 datas[:, datas_dict['current_rpm']] = datas[:, datas_dict['current_rpm']] / ControlOptions['rpmmax']
 
-for ax, subKey in zip(axs.flatten(), plotKeyList):
+
+data_units = ['', '', ' [bar]', ' [K]', ' [-]', '']
+
+for i, ax, subKey in zip(range(len(plotKeyList)), axs.flatten(), plotKeyList):
    subKeyList = getSubKeyList(subKey)
    for key in subKeyList:
-      ax.plot( times, datas[:, datas_dict[key]], label=key )
-      ax.legend()
-      ax.set(xlabel='time')
+      lab = key.replace(subKey, '')
+      lab = lab.replace('_', ' ')
+      if (i==0 and 'comp' in lab):
+         lab = lab.replace('compressor', 'comp. plenum')
 
-f.subplots_adjust(hspace=0.3)
+      if 'temperature' in subKey:
+         datas[:, datas_dict[key]] *=T_ref
+      
+      ax.plot( times, datas[:, datas_dict[key]], label=lab )
+      
+      if i>3:
+         pass
+      else:
+         ax.legend()
+      ylab = subKey.replace('_', ' ')
+      if 'rpm' in ylab:
+         ylab='rpm / rpmmax'
+      ylab += data_units[i]
+      ax.set(xlabel='time', ylabel=ylab, xlim=(0,None))
+
+f.subplots_adjust(hspace=0.3, wspace=0.3)
 f.savefig('{}/control_state.png'.format(output_path), bbox_inches='tight')
 
 f.clear()
