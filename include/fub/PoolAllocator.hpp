977409b94bc335a18a4f9a82de7f592cab351ce0
Maikel Nadolski <maikel.nadolski@fu-berlin.de>
2021-02-12 09:59:09 +0100

    Make PoolAllocator work with GCC8

--- include/fub/PoolAllocator.hpp
+++ include/fub/PoolAllocator.hpp
@@ -1,18 +1,33 @@
 #ifndef FUB_POOL_ALLOCATOR_HPP
 #define FUB_POOL_ALLOCATOR_HPP
 
+#if __has_include(<memory_resource>)
 #include <memory_resource>
-#include <vector>
+#define FUB_WITH_MEMORY_RESOURCE 1
+#elif __has_include(<experimental/memory_resource>)
+#include <experimental/memory_resource>
+#define FUB_WITH_MEMORY_RESOURCE 1
+#define FUB_WITH_EXPERIMENTAL_MEMORY_RESOURCE 1
+#endif
 #include <unordered_map>
+#include <vector>
 
 namespace fub {
 
-struct monotonic_bucket_resource : std::pmr::memory_resource {
+#ifdef FUB_WITH_EXPERIMENTAL_MEMORY_RESOURCE
+using std::experimental::pmr::memory_resource;
+#elif FUB_WITH_MEMORY_RESROUCE
+using std::pmr::memory_resource;
+#endif
+
+#ifdef FUB_WITH_MEMORY_RESOURCE
+
+struct monotonic_bucket_resource : memory_resource {
   using pointer = void*;
   using size_type = std::size_t;
 
-  monotonic_bucket_resource(std::pmr::memory_resource& resource);
-  
+  monotonic_bucket_resource(memory_resource& resource);
+
   monotonic_bucket_resource(const monotonic_bucket_resource&) = delete;
   monotonic_bucket_resource&
   operator=(const monotonic_bucket_resource&) = delete;
@@ -24,15 +39,16 @@ private:
 
   void do_deallocate(pointer p, size_type bytes, size_type alignment) override;
 
-  bool
-  do_is_equal(const std::pmr::memory_resource& other) const noexcept override;
+  bool do_is_equal(const memory_resource& other) const noexcept override;
 
 private:
-  std::pmr::memory_resource* memory_resource_;
+  memory_resource* memory_resource_;
   std::unordered_map<size_type, pointer> free_buckets_{};
   std::vector<pointer> all_pointers_{};
 };
 
-}
+#endif
+
+} // namespace fub
 
 #endif
