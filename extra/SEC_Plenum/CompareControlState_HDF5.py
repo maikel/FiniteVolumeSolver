a8ef98040c8e3bec3516ed25a33098726adc9df8
Christian Zenker <Christian.Zenker@b-tu.de>
2021-03-03 19:14:46 +0100

    update the python scripts for SEC_C Compare

--- extra/SEC_Plenum/CompareControlState_HDF5.py
+++ extra/SEC_Plenum/CompareControlState_HDF5.py
@@ -27,7 +27,7 @@ else:
 
 # bool to read all existing HDF5 files
 # this make only sense if we restarted the simulation form the last checkpoint!!
-RESTARTEDSIMULATION = True
+RESTARTEDSIMULATION = False
 
 sys.path.append(inputFilePath)
 from SEC_Plenum_Arrhenius import t_ref, ControlOptions, T_ref, rho_ref
@@ -83,7 +83,7 @@ else:
    print("Read in data from {}".format(filename_basic))
    FVS_data, FVS_times, FVS_dict = h5_load_timeseries(filename_basic)
 
-FVS_skip = 400 # skip time data to make plot clearer
+FVS_skip = 389 # skip time data to make plot clearer #380
 FVS_data = FVS_data[::FVS_skip, :]
 FVS_times = FVS_times[::FVS_skip]
 print("FVS delta t is {}".format(np.diff(FVS_times)[0]))
@@ -93,7 +93,7 @@ GT_filename = "{}/GT_time_series.txt".format(dataPath)
 
 GT_data, GT_times, GT_dict = da.get_controlState_Klein(GT_filename)
 
-GT_skip=14 # skip time data to make plot clearer
+GT_skip=14 # skip time data to make plot clearer #14
 GT_data = GT_data[::GT_skip,:]
 GT_times = GT_times[::GT_skip]
 print("GT delta t is {}".format(np.diff(GT_times)[0]))
@@ -130,7 +130,7 @@ def getMeanValue(y, x, mode='scipy_simps'):
 def CompareMeanValue(gt_data, gt_time, fvs_data, fvs_time, variable, ndig=4):
    gt_mean = getMeanValue(gt_data, gt_time)
    fvs_mean = getMeanValue(fvs_data, fvs_time)
-   means = [ ['GT Mean', 'FVS Mean', 'abs diff'],
+   means = [ ['SEC\_C Mean', 'FVS Mean', 'abs diff'],
          [gt_mean, fvs_mean, abs(gt_mean-fvs_mean)] ]
    means[1] = [ round(val, ndig) for val in means[1] ]
 
@@ -142,47 +142,50 @@ def CompareMeanValue(gt_data, gt_time, fvs_data, fvs_time, variable, ndig=4):
    return means
 
 tInit = 150.0
-GT_idx_array = (GT_times>tInit) & (GT_times<tEnd )
-FVS_idx_array = (FVS_times>tInit) & (FVS_times<tEnd )
+if tInit < tEnd:
+   GT_idx_array = (GT_times>tInit) & (GT_times<tEnd )
+   FVS_idx_array = (FVS_times>tInit) & (FVS_times<tEnd )
 
-gt_time = GT_times[GT_idx_array]
-fvs_time= FVS_times[FVS_idx_array]
+   gt_time = GT_times[GT_idx_array]
+   fvs_time= FVS_times[FVS_idx_array]
 
-meanVariables = []
-meanValues = []
+   meanVariables = []
+   meanValues = []
 
-var = 'fuel_consumption_rate'
-means = CompareMeanValue(GT_data[GT_idx_array, GT_dict[var]], gt_time, FVS_data[FVS_idx_array, FVS_dict[var]], fvs_time, var, ndig=4)
-meanVariables.append(var); meanValues.append(means)
+   var = 'fuel_consumption_rate'
+   means = CompareMeanValue(GT_data[GT_idx_array, GT_dict[var]], gt_time, FVS_data[FVS_idx_array, FVS_dict[var]], fvs_time, var, ndig=4)
+   meanVariables.append(var); meanValues.append(means)
 
-var = 'efficiency'
-means = CompareMeanValue(GT_data[GT_idx_array, GT_dict[var]], gt_time, FVS_data[FVS_idx_array, FVS_dict[var]], fvs_time, var, ndig=4)
-meanVariables.append(var); meanValues.append(means)
+   var = 'efficiency'
+   means = CompareMeanValue(GT_data[GT_idx_array, GT_dict[var]], gt_time, FVS_data[FVS_idx_array, FVS_dict[var]], fvs_time, var, ndig=4)
+   meanVariables.append(var); meanValues.append(means)
 
-var = 'turbine_pressure'
-means = CompareMeanValue(GT_data[GT_idx_array, GT_dict[var]], gt_time, FVS_data[FVS_idx_array, FVS_dict[var]], fvs_time, var, ndig=4)
-meanVariables.append(var); meanValues.append(means)
+   var = 'turbine_pressure'
+   means = CompareMeanValue(GT_data[GT_idx_array, GT_dict[var]], gt_time, FVS_data[FVS_idx_array, FVS_dict[var]], fvs_time, var, ndig=4)
+   meanVariables.append(var); meanValues.append(means)
 
-var = 'turbine_temperature'
-means = CompareMeanValue(GT_data[GT_idx_array, GT_dict[var]], gt_time, FVS_data[FVS_idx_array, FVS_dict[var]], fvs_time, var, ndig=4)
-meanVariables.append(var); meanValues.append(means)
+   var = 'turbine_temperature'
+   means = CompareMeanValue(GT_data[GT_idx_array, GT_dict[var]], gt_time, FVS_data[FVS_idx_array, FVS_dict[var]], fvs_time, var, ndig=4)
+   meanVariables.append(var); meanValues.append(means)
 
-meanVariables = [var.replace('_',' ') for var in meanVariables]
+   meanVariables = [var.replace('_',' ') for var in meanVariables]
 
-# format_row = '{:>30}'+'{:>12}'*(len(means[0]))
-# formated_table = format_row.format('', *means[0])
-# cell_text = []
-# for var, val in zip(meanVariables, meanValues):
-#    formated_table += '\n'
-#    formated_table += format_row.format(var, *val[1])
+   # format_row = '{:>30}'+'{:>12}'*(len(means[0]))
+   # formated_table = format_row.format('', *means[0])
+   # cell_text = []
+   # for var, val in zip(meanVariables, meanValues):
+   #    formated_table += '\n'
+   #    formated_table += format_row.format(var, *val[1])
 
-table_meanValues=(r'\begin{tabular}{ c | c | c | c} '
-+r' {} & {} & {} & {} \\\hline '.format(' ', *meanValues[0][0])
-+r' {} & {} & {} & {} \\\hline '.format(meanVariables[0], *meanValues[0][1])
-+r' {} & {} & {} & {} \\\hline '.format(meanVariables[1], *meanValues[1][1])
-+r' {} & {} & {} & {} \\\hline'.format(meanVariables[2], *meanValues[2][1])
-+r' {} & {} & {} & {} '.format(meanVariables[3], *meanValues[3][1])
-+r'\end{tabular}' )
+   table_meanValues=(r'\begin{tabular}{ c | c | c | c} '
+   +r' {} & {} & {} & {} \\\hline '.format(' ', *meanValues[0][0])
+   +r' {} & {} & {} & {} \\\hline '.format(meanVariables[0], *meanValues[0][1])
+   +r' {} & {} & {} & {} \\\hline '.format(meanVariables[1], *meanValues[1][1])
+   +r' {} & {} & {} & {} \\\hline'.format(meanVariables[2], *meanValues[2][1])
+   +r' {} & {} & {} & {} '.format(meanVariables[3], *meanValues[3][1])
+   +r'\end{tabular}' )
+else:
+   table_meanValues = ''
 ################# Calc Mean Values ############################
 
 def getSubKeyList(substring):
@@ -194,9 +197,9 @@ GT_data[:, GT_dict['current_rpm']] = GT_data[:, GT_dict['current_rpm']] / Contro
 print( "FVS maximum compressor_pressure = {}".format(np.max(FVS_data[FVS_t_index, FVS_dict['compressor_pressure']])) )
 print( "GT maximum compressor_pressure = {}".format(np.max(GT_data[GT_t_index, GT_dict['compressor_pressure']])) )
 
-# calculate the carnot efficiency following Kleins SEC_Code
-FVS_eff_carnot = 1.0 - 1.0/FVS_data[FVS_t_index, FVS_dict['compressor_temperature']]
-GT_eff_carnot = 1.0 - 1.0/GT_data[GT_t_index, GT_dict['compressor_temperature']]
+
+def getCarnotEff(comp_temp):
+   return (1.0-1.0/comp_temp)
 
 f, axs = plt.subplots(nrows=4, ncols=2, figsize=(23/2,20/2) )
 f.suptitle('Control Station' )
@@ -206,74 +209,86 @@ axs[0,0].text(1.5, 1.5, table_meanValues, ha="right", va="top", transform=axs[0,
 
 
 plotKeyList = ['mass_flow', 'power', 'pressure', 'temperature', 'rpm', 'fuel_consumption_rate', 'efficiency']#, 'SEC_Mode']
-plotKeyList = ['mass_flow', 'power', 'pressure', 'temperature', 'rpm', 'fuel_consumption_rate', 'efficiency', 'efficiency']
+plotKeyList = ['mass_flow', 'power', 'pressure', 'temperature', 'rpm', 'fuel_consumption_rate', 'efficiency']
 
 data_units = ['', '', ' [bar]', ' [K]', ' [-]', '', '[-]', '[-]']
-colors=['b', 'g', 'r', 'm']
+colors=['b', 'g', 'r', 'm', 'k']
+lstyles=['-', '--']
+zorder=[5,10]
 
 for i, ax, subKey in zip(range(len(plotKeyList)), axs.flatten(), plotKeyList):
    subKeyList = getSubKeyList(subKey)
    for j, key in enumerate(subKeyList):
-      if i>5: # don't destroy the labels from the last two plots
-         lab = key
-      else:
-         lab = key.replace(subKey, '')
-         lab = lab.replace('_', ' ')
-      
-      if (i==0):
-         if ('comp' in lab):
-            lab = lab.replace('compressor', 'comp. plenum')
-         elif ('turb' in lab):
-            lab = lab.replace('turbine', 'turb. plenum')
-      
-      if 'temperature' in subKey:
-         FVS_data[FVS_t_index, FVS_dict[key]] *=T_ref
-         GT_data[GT_t_index, GT_dict[key]] *=T_ref
-
-      ax.plot( FVS_times[FVS_t_index], FVS_data[FVS_t_index, FVS_dict[key]], label=lab, color=colors[j] )
-      ax.plot( GT_times[GT_t_index], GT_data[GT_t_index, GT_dict[key]], '--', zorder=10, color=colors[j] )
-
-      if i>3:
-         pass
-      else:
-         ax.legend()
-      
-      # if 'mass_flow' in subKey:
-      #    ax.set(ylim=(0,1.2))
-
-      if 'efficiency' in subKey:
-         ax.plot(FVS_times[FVS_t_index], FVS_eff_carnot, label='Carnot efficiency', color=colors[1])
-         ax.plot(GT_times[GT_t_index], GT_eff_carnot, '--', zorder=10, color=colors[1])
-         if i==6:
-            ax.legend()
-            ax.set(ylim=(0,1.0))
-      
-      ylab = subKey.replace('_', ' ')
-      if 'rpm' in ylab:
-         ylab='rpm / rpmmax'
-      ylab += data_units[i]
+      for data, time, dictionary, t_id, nr in zip([FVS_data, GT_data], [FVS_times, GT_times], [FVS_dict, GT_dict], [FVS_t_index, GT_t_index], range(2)):
+         if i>5: # don't destroy the labels from the last two plots
+            lab = key
+         else:
+            lab = key.replace(subKey, '')
+            lab = lab.replace('_', ' ')
          
-      ax.set(xlabel='time', ylabel=ylab, xlim=(FVS_times[0], None))
-      ax.grid(True)
+         if (i==0):
+            if ('comp' in lab):
+               lab = lab.replace('compressor', 'comp. plenum')
+            elif ('turb' in lab):
+               lab = lab.replace('turbine', 'turb. plenum')
+         
+         
+         ax.plot( time[t_id], data[t_id, dictionary[key]], lstyles[nr], label=lab, zorder=zorder[nr], color=colors[j] )
+
+         if i>3:
+            pass
+         else:
+            ax.legend()
+            handles, labels = ax.get_legend_handles_labels()
+            # print(labels)
+            by_label = dict(zip(labels[::2], handles[::2]))
+            ax.legend(by_label.values(), by_label.keys())
+         
+
+         if 'efficiency' in subKey:
+            ax.plot(time[t_id], getCarnotEff(data[t_id, dictionary['compressor_temperature']]), lstyles[nr], 
+                              label='Carnot_efficiency'.replace('_', ' '), zorder=zorder[nr], color=colors[1])
+         
+         ylab = subKey.replace('_', ' ')
+         if 'rpm' in ylab:
+            ylab='rpm / rpmmax'
+         ylab += data_units[i]
+            
+         ax.set(xlabel='time', ylabel=ylab, xlim=(FVS_times[0], None))
+         ax.grid(True)
 
 ax = axs.flatten()
 
-# FVS_turb_density = FVS_data[:, FVS_dict['turbine_pressure']] / FVS_data[:, FVS_dict['turbine_temperature']] * rho_ref
-# FVS_comp_density = FVS_data[:, FVS_dict['compressor_pressure']] / FVS_data[:, FVS_dict['compressor_temperature']] * rho_ref
+# make legend for efficiency
+handles, labels = ax[6].get_legend_handles_labels()
+ax[6].legend(handles=handles[:2], labels=labels[:2])
+ax[6].set(ylim=(0,1.0))
+               
+# make figure legend FVS: solid line; SEC_C: dashed line
+handles, labels = ax[4].get_legend_handles_labels()
+labels = ['FVS', 'SEC\_C']
+f.legend(handles=handles , labels=labels,loc='upper center', 
+             bbox_to_anchor=(0.5, +0.08),fancybox=False, shadow=False, ncol=4)
 
-# GT_turb_density = GT_data[:, GT_dict['turbine_pressure']] / GT_data[:, GT_dict['turbine_temperature']] * rho_ref
-# GT_comp_density = GT_data[:, GT_dict['compressor_pressure']] / GT_data[:, GT_dict['compressor_temperature']] * rho_ref
 
-# ax[-1].plot(FVS_times[FVS_t_index], FVS_comp_density[FVS_t_index], label='compressor', color=colors[0])
-# ax[-1].plot( GT_times[GT_t_index], GT_comp_density[GT_t_index], '--', zorder=10, color=colors[0] )
-# ax[-1].plot(FVS_times[FVS_t_index], FVS_turb_density[FVS_t_index], label='turbine', color=colors[1])
-# ax[-1].plot( GT_times[GT_t_index], GT_turb_density[GT_t_index], '--', zorder=10, color=colors[1] )
-# ax[-1].set(xlabel='time', ylabel='density '+r'$\left[ kg/m^3 \right]$', xlim=(FVS_times[0], None))
-# ax[-1].legend()
+FVS_turb_density = FVS_data[:, FVS_dict['turbine_pressure']] / FVS_data[:, FVS_dict['turbine_temperature']] * rho_ref
+FVS_comp_density = FVS_data[:, FVS_dict['compressor_pressure']] / FVS_data[:, FVS_dict['compressor_temperature']] * rho_ref
+
+GT_turb_density = GT_data[:, GT_dict['turbine_pressure']] / GT_data[:, GT_dict['turbine_temperature']] * rho_ref
+GT_comp_density = GT_data[:, GT_dict['compressor_pressure']] / GT_data[:, GT_dict['compressor_temperature']] * rho_ref
+
+ax[-1].plot(FVS_times[FVS_t_index], FVS_comp_density[FVS_t_index], label='compressor', color=colors[0])
+ax[-1].plot( GT_times[GT_t_index], GT_comp_density[GT_t_index], '--', zorder=10, color=colors[0] )
+ax[-1].plot(FVS_times[FVS_t_index], FVS_turb_density[FVS_t_index], label='turbine', color=colors[1])
+ax[-1].plot( GT_times[GT_t_index], GT_turb_density[GT_t_index], '--', zorder=10, color=colors[1] )
+ax[-1].set(xlabel='time', ylabel='density '+r'$\left[ kg/m^3 \right]$', xlim=(FVS_times[0], None))
+ax[-1].legend()
 
 
 f.subplots_adjust(hspace=0.3, wspace=0.3)
-f.savefig('{}/control_state_FVS_GT.png'.format(output_path), bbox_inches='tight')
+f.savefig('{}/control_state_Compare.png'.format(output_path), bbox_inches='tight')
+f.savefig('{}/control_state_Compare.pdf'.format(output_path), bbox_inches='tight')
+
 
 f.clear()
 plt.close(f)
