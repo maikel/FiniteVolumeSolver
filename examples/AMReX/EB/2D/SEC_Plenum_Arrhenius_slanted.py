b52a9abe91fabf3df053441ffdad4f1e46ac9497
Christian Zenker <Christian.Zenker@b-tu.de>
2021-03-31 12:23:31 +0200

    modified input files from normal and slanted SEC_Plenum_Arrheinus drivers for comparative test case

--- examples/AMReX/EB/2D/SEC_Plenum_Arrhenius_slanted.py
+++ examples/AMReX/EB/2D/SEC_Plenum_Arrhenius_slanted.py
@@ -1,47 +1,40 @@
 import math
 
-tube_n_cells = 256
+tube_n_cells = 200
 # plenum_x_n_cells = 128
 tube_blocking_factor = 8
-plenum_blocking_factor = 8
+plenum_blocking_factor = 32
 
+angle = 0. / 180. * math.pi
 massflow_cor = 0.24 #%MASSFLOW%
 diffusion = 3.0 #%Diffusion%
-outputPath = 'SEC_Plenum_Arrhenius' # '%OUTPUT%'
+outputPath = 'slanted_angle0_vol40_y0.48' # '%OUTPUT%'
 
 mode = 3 #%MODE%
 boundary_condition = 'TurbineMassflowBoundaries' # '%BOUNDARY_CONDITION%'
 
 n_level = 1
 
-n_tubes = 6
-r_tube = 0.015
+y0s = [0.0]
+n_tubes = len(y0s)
 
+r_tube = 0.015
 D = 2.0 * r_tube
 
-r_inner = 0.5 * 0.130
-r_outer = 0.5 * 0.2
-r_tube_center = 2.0 * r_inner
-alpha = 2.0 * math.pi / n_tubes
-
-inlet_length = 10 * D # [m]
-
-plenum_y_lower = - 1.0
-plenum_y_upper = + 1.0
-plenum_y_length = plenum_y_upper - plenum_y_lower
-
-TVolRPlen = 20.0 * D
-plenum_x_upper = TVolRPlen / plenum_y_length
-plenum_x_lower = -0.3
-plenum_x_length = plenum_x_upper - plenum_x_lower
-
-plenum_length = plenum_x_upper # [m]
+inlet_length = 10. * D # [m]
 tube_length = 1.0 # [m]
+tube_domain_length = tube_length - inlet_length
 
 plenum_max_grid_size = max(plenum_blocking_factor, 1024)
+plenum_y_lower = - 0.48
+plenum_y_upper = + 0.48
+plenum_y_length = plenum_y_upper - plenum_y_lower
 
+TVolRPlen = 2.0 * 20.0 * D
+plenum_x_upper = TVolRPlen / plenum_y_length
+
+plenum_length = plenum_x_upper # [m]
 plenum_domain_length = plenum_length + inlet_length
-tube_domain_length = tube_length - inlet_length
 
 tube_over_plenum_length_ratio = tube_domain_length / plenum_domain_length
 plenum_over_tube_length_ratio = 1.0 / tube_over_plenum_length_ratio
@@ -49,6 +42,10 @@ plenum_over_tube_length_ratio = 1.0 / tube_over_plenum_length_ratio
 plenum_x_n_cells = tube_n_cells * plenum_over_tube_length_ratio
 plenum_x_n_cells -= plenum_x_n_cells % plenum_blocking_factor
 plenum_x_n_cells = int(plenum_x_n_cells)
+
+plenum_dx = plenum_domain_length / plenum_x_n_cells
+plenum_x_lower = -inlet_length #-plenum_dx
+plenum_x_length = plenum_x_upper - plenum_x_lower
 
 plenum_z_lower = plenum_y_lower
 plenum_z_upper = plenum_y_upper
@@ -66,23 +63,29 @@ plenum_z_n_cells = plenum_x_n_cells * plenum_z_over_x_ratio
 plenum_z_n_cells -= plenum_z_n_cells % plenum_blocking_factor
 plenum_z_n_cells = int(plenum_z_n_cells)
 
-# tube_n_cells = plenum_x_n_cells * tube_over_plenum_length_ratio
-# tube_n_cells -= tube_n_cells % tube_blocking_factor
-# tube_n_cells = int(tube_n_cells)
+tube_dx = tube_domain_length/ tube_n_cells
+plenum_dx = plenum_domain_length / plenum_x_n_cells
+plenum_dy = plenum_y_length / plenum_y_n_cells
+plenum_dz = plenum_z_length / plenum_z_n_cells
 
-# y0s = [-1.0/3.0, 0.0, +1.0/3.0]
-y0s = [0.0]
-n_tubes = len(y0s)
+formatter = (4*"{:>12}")
+
+print(formatter.format('', 'ncells', 'length', 'delta_x'))
+print(formatter.format('Tube_x', tube_n_cells, tube_domain_length, round(tube_dx, 5)))
+print(formatter.format('plenum_x', plenum_x_n_cells, plenum_domain_length, round(plenum_dx, 5)))
+print(formatter.format('plenum_y', plenum_y_n_cells, plenum_y_length, round(plenum_dy, 5)))
+print(formatter.format('plenum_z', plenum_z_n_cells, plenum_z_length, round(plenum_dz, 5)))
+print("total cells = {}".format(tube_n_cells+ (plenum_x_n_cells*plenum_y_n_cells)))
 
 RunOptions = {
-  'cfl': 0.1,# / float(tube_n_cells / 64),
-  'final_time': 300.,
+  'cfl': 0.1125,# / float(tube_n_cells / 64),
+  'final_time': 0.2, #300.,
   'max_cycles': -1,
   'do_backup': 0
 }
 
 LogOptions = {
-  'file_template': '{rank}.txt',
+  'file_template': '{}/000.log'.format(outputPath),
   'channel_blacklist': ['TurbineMassflowBoundary']
 }
 
@@ -90,7 +93,7 @@ FluxMethod = {
   # HLLEM, HLLEM_Larrouturou
   'base_method': 'HLLEM_Larrouturou',
   # Upwind, MinMod, VanLeer
-  'limiter': 'MinMod',
+  'limiter': 'VanLeer',
   # Conservative, Primitive, Characteristics
   'reconstruction': 'Characteristics'
 }
@@ -116,8 +119,8 @@ ArrheniusKinetics = {
 DiffusionSourceTerm = {
   'mul': diffusion
 }
-R_ref = 287.
-p_ref = 101325.
+R_ref = 287.4
+p_ref = 10_000.
 T_ref = 300.
 L_ref = 1.0
 rho_ref = p_ref / T_ref / R_ref
@@ -129,7 +132,8 @@ p0 = 2.0
 rho0 = math.pow(p0, 1.0 / gamma)
 T0 = p0 / rho0
 p = 0.95 * p0
-T = T0 + ArrheniusKinetics['Q'] * (gamma - 1.0)
+# T = T0 + ArrheniusKinetics['Q'] * (gamma - 1.0)
+T = 11.290743302923245
 rho = p / T
 
 # checkpoint = '/srv/public/Maikel/FiniteVolumeSolver/build_2D-Debug/Checkpoint/000000005'
@@ -154,14 +158,14 @@ ControlOptions = {
   'target_pressure_compressor' : 6.0,
   'checkpoint': checkpoint,
   # Tube surface
-  'surface_area_tube_inlet': (n_tubes * Area(-1.0) * D),
-  'surface_area_tube_outlet': (n_tubes * Area(0.0) * D),
+  'surface_area_tube_inlet': (n_tubes * Area(-1.0) * D) / D,
+  'surface_area_tube_outlet': (n_tubes * Area(0.0) * D) / D,
   # Turbine volumes and surfaces
-  'volume_turbine_plenum': TVolRPlen,
-  'surface_area_turbine_plenum_to_turbine': plenum_y_length,
+  'volume_turbine_plenum': TVolRPlen / D,
+  'surface_area_turbine_plenum_to_turbine': 4.0 * D / D,
   # Compressor volumes and surfaces
-  'volume_compressor_plenum': TVolRPlen,
-  'surface_area_compressor_to_compressor_plenum': 8.0 * D,
+  'volume_compressor_plenum': TVolRPlen / D,
+  'surface_area_compressor_to_compressor_plenum': 8.0 * D / D,
 }
 
 def ToCellIndex(x, xlo, xhi, ncells):
@@ -170,76 +174,17 @@ def ToCellIndex(x, xlo, xhi, ncells):
   i = int(x_rel * ncells)
   return i
 
-Plenum = {
-  'checkpoint': checkpoint,
-  'GridGeometry': {
-    'cell_dimensions': [plenum_x_n_cells, plenum_y_n_cells, plenum_z_n_cells],
-    'coordinates': {
-      'lower': [plenum_x_lower, plenum_y_lower, plenum_z_lower],
-      'upper': [plenum_x_upper, plenum_y_upper, plenum_z_upper],
-    },
-    # 'periodicity': [0, 1, 0]
-  },
-  'PatchHierarchy': {
-    'max_number_of_levels': n_level, 
-    'blocking_factor': [plenum_blocking_factor, plenum_blocking_factor, plenum_blocking_factor],
-    'max_grid_size': [plenum_max_grid_size, plenum_max_grid_size, plenum_max_grid_size],
-    'ngrow_eb_level_set': 9,
-    'remove_covered_grids': False,
-    'n_proper': 1,
-    'n_error_buf': [0, 0, 0]
-  },
-  'IntegratorContext': {
-    'scratch_gcw': 2,
-    'flux_gcw': 0,
-  },
-  'FluxMethod': FluxMethod,
-  'InletGeometries': [{
-    'r_start': 4.0 * r_tube,
-    'r_end': 4.0 * r_tube,
-    'y_0': y_0,
-    'height': inlet_length,
-    'angle':  60. / 180. * math.pi
-  } for y_0 in y0s],
-  'InitialCondition': {
-    'left': {
-      'density': rho,
-      'temperature': T,
-      'pressure': p
-    },
-    'right': {
-      'density': rho,
-      'temperature': T,
-      'pressure': p
-    },
-  }
-}
-
-Plenum[boundary_condition] = {
-  'boundary_section': { 
-    'lower': [plenum_x_n_cells, -4, 0], 
-    'upper': [plenum_x_n_cells + 3, plenum_y_n_cells + 3, 0] 
-    },
-  'mode': mode,
-  'coarse_average_mirror_box': {
-    'lower': [plenum_x_n_cells - 1, 0, 0],
-    'upper': [plenum_x_n_cells - 1, plenum_y_n_cells - 1, 0]
-  },
-  'relative_surface_area': ControlOptions['surface_area_turbine_plenum_to_turbine'], 
-  'massflow_correlation': massflow_cor, # 0.06 * 4.0,
-}
-
-def TubeCenterPoint(x0, y0):
+def GetCenterPoint(x0, y0):
   return [x0, y0, 0.0]
 
 def LowerX(x0, y0):
-  center = TubeCenterPoint(x0, y0)
+  center = GetCenterPoint(x0, y0)
   center[1] -= r_tube
   center[2] -= r_tube
   return center
 
 def UpperX(x0, y0):
-  center = TubeCenterPoint(x0, y0)
+  center = GetCenterPoint(x0, y0)
   center[1] += r_tube
   center[2] += r_tube
   return center
@@ -256,8 +201,97 @@ def BoxWhichContains(real_box):
   jEnd = ToCellIndex(real_box[1][1], plenum_y_lower, plenum_y_upper, plenum_y_n_cells)
   return { 'lower': [i0, j0, 0], 'upper': [iEnd, jEnd, 0] }
 
+def BoxWhichContains_withGhostcells(real_box, gcw_x, gcw_y):
+  i0 = ToCellIndex(real_box[0][0], plenum_x_lower, plenum_x_upper, plenum_x_n_cells)
+  iEnd = ToCellIndex(real_box[1][0], plenum_x_lower, plenum_x_upper, plenum_x_n_cells)
+  j0 = ToCellIndex(real_box[0][1], plenum_y_lower, plenum_y_upper, plenum_y_n_cells)
+  jEnd = ToCellIndex(real_box[1][1], plenum_y_lower, plenum_y_upper, plenum_y_n_cells)
+  return { 'lower': [i0-gcw_x[0], j0-gcw_y[0], 0], 'upper': [iEnd+gcw_x[1]-1, jEnd+gcw_y[1]-1, 0] }
+
+Plenum = {
+  'checkpoint': checkpoint,
+  'GridGeometry': {
+    'cell_dimensions': [plenum_x_n_cells, plenum_y_n_cells, plenum_z_n_cells],
+    'coordinates': {
+      'lower': [plenum_x_lower, plenum_y_lower, plenum_z_lower],
+      'upper': [plenum_x_upper, plenum_y_upper, plenum_z_upper],
+    },
+    'periodicity': [0, 1, 0]
+  },
+  'PatchHierarchy': {
+    'max_number_of_levels': n_level, 
+    'blocking_factor': [plenum_blocking_factor, plenum_blocking_factor, plenum_blocking_factor],
+    'max_grid_size': [plenum_max_grid_size, plenum_max_grid_size, plenum_max_grid_size],
+    'ngrow_eb_level_set': 9,
+    'remove_covered_grids': False,
+    'n_proper': 1,
+    'n_error_buf': [0, 0, 0]
+  },
+  'IntegratorContext': {
+    'scratch_gcw': 4,
+    'flux_gcw': 2,
+  },
+  'FluxMethod': FluxMethod,
+  'InletGeometries': [{
+    'r_start': 4.0 * r_tube,
+    'r_end': 4.0 * r_tube,
+    'y_0': y_0,
+    'height': inlet_length,
+    'angle':  angle # 60. / 180. * math.pi
+  } for y_0 in y0s],
+  'InitialCondition': {
+    'left': {
+      'density': rho,
+      'temperature': T,
+      'pressure': p
+    },
+    'right': {
+      'density': rho,
+      'temperature': T,
+      'pressure': p
+    },
+  }
+}
+
+Plenum_scratch_gcw = Plenum['IntegratorContext']['scratch_gcw']
+
+# get Boundarybox for the Plenum (upper side in x-Direction)
+plenum_y_midpoint = (plenum_y_upper + plenum_y_lower)/2.0
+def PlenumBoundaryBox(y0):
+  return BoxWhichContains_withGhostcells(DomainAroundPoint( GetCenterPoint(plenum_x_upper, y0), [0.0, -2.*D], [0.0, +2.*D]), 
+            [0,Plenum_scratch_gcw], [0, 0])
+
+## if plenum boundary goes over the whole y-range
+# def PlenumBoundaryBox(y0):
+#   return BoxWhichContains_withGhostcells(DomainAroundPoint( GetCenterPoint(plenum_x_upper, y0), [0.0, plenum_y_lower], [0.0, plenum_y_upper]), 
+#             [0,Plenum_scratch_gcw], [Plenum_scratch_gcw, Plenum_scratch_gcw])
+
+
+# get the Box for the Plenum where the massflow is accumulated (upper side in x-Direction)
+def PlenumCoarseAverageMirrorBox(y0):
+  return BoxWhichContains_withGhostcells(DomainAroundPoint( GetCenterPoint(plenum_x_upper, y0), [0.0, -2.*D], [0.0, +2.*D]), 
+            [1,0], [0, 0])
+
+Plenum[boundary_condition] = {
+  # 'boundary_section': { 
+  #   'lower': [plenum_x_n_cells, - Plenum_scratch_gcw, 0], 
+  #   'upper': [plenum_x_n_cells + Plenum_scratch_gcw - 1, plenum_y_n_cells + Plenum_scratch_gcw - 1, 0] 
+  #   },
+  'boundary_section': PlenumBoundaryBox(plenum_y_midpoint),
+  'mode': mode,
+  # 'coarse_average_mirror_box': {
+  #   'lower': [plenum_x_n_cells - 1, 0, 0],
+  #   'upper': [plenum_x_n_cells - 1, plenum_y_n_cells - 1, 0]
+  # },
+  'coarse_average_mirror_box': PlenumCoarseAverageMirrorBox(plenum_y_midpoint),
+  'relative_surface_area': ControlOptions['surface_area_turbine_plenum_to_turbine'], 
+  'massflow_correlation': 0.06 * 4.0,
+}
+
+
+# get Mirrorbox for each Tube where they are connected with the plenum
 def PlenumMirrorBox(y0):
-  return BoxWhichContains(DomainAroundPoint(TubeCenterPoint(plenum_x_lower, y0), [0.0, -1.0], [-0.9 * plenum_x_lower, +1.0]))
+  return BoxWhichContains(DomainAroundPoint(GetCenterPoint(plenum_x_lower, y0), [0.0, -1.0], [inlet_length, +1.0]))
 
 
 Tube_FluxMethod = FluxMethod
@@ -266,7 +300,7 @@ Tube_FluxMethod['area_variation'] = Area
 Tubes = [{
   'checkpoint': checkpoint if checkpoint == '' else '{}/Tube_{}'.format(checkpoint, i),
   # 'buffer': 0.06,
-  'initially_filled_x': 0.1,
+  'initially_filled_x': 0.4,
   'FluxMethod': Tube_FluxMethod,
   'plenum_mirror_box': PlenumMirrorBox(y_0),
   'GridGeometry': {
@@ -288,9 +322,14 @@ Tubes = [{
     'n_error_buf': [4, 0, 0]
   },
   'IntegratorContext': {
-    'scratch_gcw': 4,
-    'flux_gcw': 0,
+    'scratch_gcw': 6,
+    'flux_gcw': 2,
   },
+  'InflowOptionsSEC' : {
+    'SEC_buffer': 0.06, # 0.06
+    'SEC_tti': 1.2, # 1.2
+    'SEC_timin': 0.1, # 0.1
+  }
 } for (i, y_0) in enumerate(y0s)]
 
 mode_names = ['cellwise', 'average_mirror_state', 'average_ghost_state', 'average_massflow']
@@ -325,9 +364,8 @@ Output = {
   },
   {
     'type': 'Plotfiles',
-    #'directory': '/group/ag_klima/SFB1029_C01/SEC_Plenum/{}/Plotfiles/'.format(mode_names[Plenum['TurbineMassflowBoundaries'][0]['mode']]),
     'directory': '{}/Plotfiles'.format(outputPath),
-    'intervals': [plenum_intervals],
+    'intervals': [0.001],
     #'frequencies': [1]
   }, {
     #'#type': 'Checkpoint',
@@ -342,7 +380,7 @@ Output = {
   {
     'type': 'Checkpoint',
     'intervals': [1.0],
-    'directory': 'Checkpoint/'
+    'directory': '{}/Checkpoint/'.format(outputPath)
   }
   ]
 }
