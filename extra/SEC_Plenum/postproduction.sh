16012abeed727d6dee206d90edb0c6cfbf02c225
Christian Zenker <Christian.Zenker@b-tu.de>
2022-09-17 20:31:53 +0200

    SEC-Plenum_Arrhenius symmetry test stuff

---
+++ extra/SEC_Plenum/postproduction.sh
@@ -0,0 +1,7 @@
+CaseName='PATH/TO/FILES' # here 
+NCPUs=8 # for parallel plotting
+
+time python3 PlotTube_HDF5.py $CaseName
+time python3 PlotControlState_HDF5.py $CaseName
+time python3 PlotPlenum_HDF5.py $CaseName --parallel=$NCPUs # dauert lange
+time python3 PlotPlenumStats.py $CaseName
